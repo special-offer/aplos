84e4d712b7d6b441bb82aa2d0631d734a67a9a6b
Stefan Bowerman <stefbowerman@gmail.com>
2020-05-17 01:05:00 -0700

    More work on the password page

--- src/_scripts/sections/password.js
+++ src/_scripts/sections/password.js
@@ -9,12 +9,16 @@ const selectors = {
 
 const $body = $(document.body);
 
+const INTERACTION_MS = 300; // Give the user a little bit of time to move between input + submit
+
 export default class PasswordSection extends BaseSection {
   constructor(container) {
     super(container, 'password');
 
-    this.$form = $(selectors.form, this.$container);
-    this.$input = this.$form.find('input[type="email"]');
+    this.interactionTimeout = null;
+    this.isSubmitting       = false;
+
+    this.$form        = $(selectors.form, this.$container);
     this.$formMessage = $(selectors.formMessage, this.$container);
 
     this.klaviyoForm = new AJAXKlaviyoForm(this.$form, {
@@ -23,28 +27,50 @@ export default class PasswordSection extends BaseSection {
       onSubscribeSuccess: this.onSubscribeSuccess.bind(this),
       onSubscribeFail: this.onSubscribeFail.bind(this),
       onSubmitFail: this.onSubmitFail.bind(this)
-    })
+    });
 
-    this.$input.one('focus', () => $body.addClass('is-interacted'));
+    this.$input  = this.klaviyoForm.$input;
+    this.$submit = this.klaviyoForm.$submit;
+
+    this.$input.on({
+      focus: () => this.setInteracting(true),
+      blur: () => this.setInteracting(false)
+    });
+    this.$submit.on({
+      'focus mouseenter': () => this.setInteracting(true),
+      'blur': () => this.setInteracting(false)
+    });
+  }
+
+  setInteracting(interacting) {
+    // if (this.isSubmitting) return; // Maybe?
+
+    if (interacting) {
+      clearTimeout(this.interactionTimeout);
+      $body.addClass('is-interacting');
+    }
+    else {
+      this.interactionTimeout = setTimeout(() => {
+        $body.removeClass('is-interacting');
+      }, INTERACTION_MS);
+    }
   }
 
   onSubscribeSuccess(response) {
-    console.log('success');
-    console.log(response);
-
+    // console.log('success');
+    // console.log(response);
     const isSubscribed = response && response.data && response.data.is_subscribed;
     this.$formMessage.text(this.$formMessage.data(isSubscribed ? 'message-already-subscribed' : 'message-success'));
   }
 
   onSubscribeFail(response) {
-    console.log('fail');
-    console.log(response);
+    // console.log('fail');
+    // console.log(response);
     this.$formMessage.text(this.$formMessage.data('message-fail'));
   }
 
   onSubmitFail(errors) {
-    const e = Array.isArray(errors) ? errors.join('  ') : errors
-
-    console.log(e)
+    // const e = Array.isArray(errors) ? errors.join('  ') : errors;
+    // console.log(e)
   }
 }
