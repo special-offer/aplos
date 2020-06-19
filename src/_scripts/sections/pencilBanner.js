6b003558707d80c527114a08e62bad2ab1a0f0b3
Stefan Bowerman <stefbowerman@gmail.com>
2020-06-18 18:07:57 -0700

    Minor updates to header + pencil banner

--- src/_scripts/sections/pencilBanner.js
+++ src/_scripts/sections/pencilBanner.js
@@ -1,8 +1,23 @@
-import $ from 'jquery'; // eslint-disable-line no-unused-vars
+import $ from 'jquery';
+import { isThemeEditor } from '../core/utils';
 import BaseSection from './base';
+
+const selectors = {
+  close: '[data-pencil-banner-close]'
+};
 
 export default class PencilBannerSection extends BaseSection {
   constructor(container) {
     super(container, 'pencilBanner');
+
+    this.$container.on('click', selectors.close, this.onCloseClick.bind(this));
+  }
+
+  onCloseClick(e) {
+    e.preventDefault();
+
+    if(isThemeEditor()) return;
+
+    console.log('close pencil banner');
   }
 }
