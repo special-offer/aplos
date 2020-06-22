977f1f182e114015ff80ed51d340f07a90fd6c91
Stefan Bowerman <stefbowerman@gmail.com>
2020-06-22 15:21:29 -0700

    Majority of drawer work completed

--- src/_scripts/sections/ajaxCart.js
+++ src/_scripts/sections/ajaxCart.js
@@ -3,33 +3,27 @@ import BaseSection from './base';
 import CartAPI from '../core/cartAPI';
 import { getQueryParams } from '../core/utils';
 import AJAXFormManager from '../managers/ajaxForm';
-import AJAXCartUI from '../ui/ajaxCart';
+import AJAXCartDrawer from '../ui/ajaxCartDrawer';
 
 const $window = $(window);
 
 /**
  * Ajax Cart Section Script
  * ------------------------------------------------------------------------------
- * All logic is handled cia CartAPI or AJAXCartUI
- * This file is strictly for handling section settings and theme editor interactions
+ * All logic is handled via CartAPI or AJAXCartDrawer
+ * This file is strictly for handling section settings/instantiation and theme editor interactions
  *
  */
 export default class AJAXCartSection extends BaseSection {
   constructor(container) {
     super(container, 'ajaxCart');
 
-    // Create a new instance of the cart UI.
-    // Pass in any variables used by the Handlebars template that aren't part of the cart object
-    this.ajaxCartUI = new AJAXCartUI();
+    this.ajaxCartDrawer = new AJAXCartDrawer(this.$container.find('.drawer').first());
 
     // Store callbacks so we can remove them later
     this.callbacks = {
-      changeSuccess: (e) => {
-        this.ajaxCartUI.onChangeSuccess(e.cart);
-      },
-      changeFail: (e) => {
-        this.ajaxCartUI.onChangeFail(e.message);
-      }
+      changeSuccess: e => this.ajaxCartDrawer.onChangeSuccess(e.cart),
+      changeFail:    e => this.ajaxCartDrawer.onChangeFail(e.message)
     };
 
     $window.on(AJAXFormManager.events.ADD_SUCCESS, this.callbacks.changeSuccess);
@@ -37,25 +31,25 @@ export default class AJAXCartSection extends BaseSection {
 
     // Make sure we get the latest cart data when this initializes
     CartAPI.getCart().then((cart) => {
-      this.ajaxCartUI.render(cart);
+      this.ajaxCartDrawer.render(cart);
 
       // If redirected from the cart, show the ajax cart after a short delay
       if (getQueryParams().cart) {
-        this.ajaxCartUI.open();
+        this.ajaxCartDrawer.show();
       }
     });
   }
 
   onSelect() {
-    this.ajaxCartUI.open();
+    this.ajaxCartDrawer.show();
   }
 
   onDeselect() {
-    this.ajaxCartUI.close();
+    this.ajaxCartDrawer.hide();
   }
 
   onUnload() {
-    this.ajaxCartUI && this.ajaxCartUI.destroy(); // Need to destroy to clean up body / window event listeners
+    this.ajaxCartDrawer.destroy(); // Need to destroy to clean up body / window event listeners
     $window.off(AJAXFormManager.events.ADD_SUCCESS, this.callbacks.changeSuccess);
     $window.off(AJAXFormManager.events.ADD_FAIL, this.callbacks.changeFail);
   }
