30b356c6c26f4cbd6344b9f4e614d9317aeed08f
Stefan Bowerman <stefbowerman@gmail.com>
2020-06-25 12:40:16 -0700

    Combine header + pencil banner into the same section

--- src/_scripts/sections/header.js
+++ src/_scripts/sections/header.js
@@ -1,5 +1,9 @@
 import $ from 'jquery';
 import { throttle } from 'throttle-debounce';
+import { 
+  isThemeEditor,
+  whichTransitionEnd
+} from '../core/utils';
 import BaseSection from './base';
 import AJAXCartDrawer from '../ui/ajaxCartDrawer';
 
@@ -8,13 +12,17 @@ const $body   = $(document.body);
 
 const selectors = {
   header: '[data-header]',
+  pencilBanner: '[data-pencil-banner]',
+  headerMain: '[data-header-main]', // @TODO - Come up with a better name for this?
   cartBadge: '[data-cart-badge]',
-  cartBadgeCount: '[data-cart-badge-count]'
+  cartBadgeCount: '[data-cart-badge-count]',
+  closePencilBanner: '[data-pencil-banner-close]'
 };
 
 const classes = {
-  headerFixed: 'is-fixed',
-  siteHasFixedHeader: 'site-fixed-header',
+  headerHidden: 'is-hidden',
+  headerIsShifting: 'is-shifting',
+  headerMainFixed: 'is-fixed',
   cartBadgeHasItems: 'has-items'
 };
 
@@ -22,23 +30,42 @@ export default class HeaderSection extends BaseSection {
   constructor(container) {
     super(container, 'header');
 
-    this.$el = $(selectors.header, this.$container);
+    this.$header         = $(selectors.header, this.$container);
+    this.$pencilBanner   = $(selectors.pencilBanner, this.$container);
+    this.$headerMain     = $(selectors.headerMain, this.$container);
     this.$cartBadge      = $(selectors.cartBadge, this.$container);
     this.$cartBadgeCount = $(selectors.cartBadgeCount, this.$container);
 
-    // We pass in the fixed behavior as a class on the body of the site
-    if ($body.hasClass(classes.siteHasFixedHeader)) {
-      $window.on('scroll', throttle(50, this.onScroll.bind(this)));
-      this.onScroll(); // hit this one time on init to make sure everything is good
-    }
+    this.transitionEndEvent = whichTransitionEnd();
 
-    $window.on(AJAXCartDrawer.events.RENDER, this.onAJAXCartRender.bind(this));
+    // Cache these values because we use them in the scroll handler
+    this.prevScrollTop      = 0;
+    this.headerHeight       = 0;
+    this.pencilBannerHeight = 0;
+
+    // Bind these events so we can remove them
+    this.throttledOnScroll = throttle(50, this.onScroll.bind(this));
+    this.throttledOnResize = throttle(100, this.onResize.bind(this));
+    this.onAJAXCartRender = this.onAJAXCartRender.bind(this)
+
+    this.$container.on('click', selectors.closePencilBanner, this.onClosePencilBannerClick.bind(this));
+    $window.on(AJAXCartDrawer.events.RENDER, this.onAJAXCartRender);
+    $window.on('scroll', this.throttledOnScroll);
+    $window.on('resize', this.throttledOnResize);
+
+    this.cacheDimensions(); // need these dimensions set before we call any scroll handlers
+    this.onScroll(); // hit this one time on init to make sure everything is good    
   }
 
-  onAJAXCartRender(e) {
-    if (e.cart) {
-      this.updateCartCount(e.cart.item_count);
-    }
+  onUnload() {
+    $window.off(AJAXCartDrawer.events.RENDER, this.onAJAXCartRender);
+    $window.off('scroll', this.throttledOnScroll);
+    $window.off('resize', this.throttledOnResize);
+  }
+
+  cacheDimensions() {
+    this.headerHeight       = this.$header.outerHeight();
+    this.pencilBannerHeight = this.$pencilBanner.is(':visible') ? this.$pencilBanner.outerHeight() : 0;
   }
 
   /**
@@ -51,17 +78,47 @@ export default class HeaderSection extends BaseSection {
     this.$cartBadge.toggleClass(classes.cartBadgeHasItems, count > 0);
   }
 
-  scrollCheck() {
-    // Do measurements outside of rAF.
-    const scrollTop = $window.scrollTop();
-    const actualOffset = this.$container.offset().top - this.$el.outerHeight();
-    const toggle = !(scrollTop < actualOffset);
-
-    // Do DOM updates inside.
-    requestAnimationFrame(() => this.$el.toggleClass(classes.headerFixed, toggle));
+  onAJAXCartRender(e) {
+    if (e.cart) {
+      this.updateCartCount(e.cart.item_count);
+    }
   }
 
   onScroll() {
-    this.scrollCheck();
+    // Do measurements outside of rAF.
+    const scrollTop  = $window.scrollTop();
+    const fixed      = scrollTop > this.pencilBannerHeight;
+    const hideHeader = scrollTop > this.prevScrollTop && scrollTop > this.headerHeight; //  going down and scrolled past header natural height
+
+    // Do DOM updates inside.
+    requestAnimationFrame(() => {
+      this.$header.toggleClass(classes.headerHidden, hideHeader);
+      this.$headerMain.toggleClass(classes.headerMainFixed, fixed);
+    });
+
+    this.prevScrollTop = scrollTop;
+  }
+
+  onResize() {
+    this.cacheDimensions();
+  }
+
+  onClosePencilBannerClick(e) {
+    e.preventDefault();
+
+    if (isThemeEditor()) return;
+
+    this.$header.one(this.transitionEndEvent, () => {
+      this.$header
+        .css('top', '');
+        .removeClass(classes.headerIsShifting);
+      this.$pencilBanner.hide();
+      this.cacheDimensions();
+      this.onScroll();
+    });
+
+    this.$header
+      .addClass(classes.headerIsShifting)
+      .css('top', this.pencilBannerHeight * -1);
   }
 }
